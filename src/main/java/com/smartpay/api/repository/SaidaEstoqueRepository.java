9d72312977bf0ea2096df1a794ebfe17913b7e3b
ErickBenardo2706 <erickbernardodasilva96@gmail.com>
2026-04-16 22:51:06 -0300

    feat: adicionado rest de  saida estoque

---
+++ src/main/java/com/smartpay/api/repository/SaidaEstoqueRepository.java
@@ -0,0 +1,8 @@
+package com.smartpay.api.repository;
+
+import com.smartpay.api.model.SaidaEstoque;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaidaEstoqueRepository extends JpaRepository<SaidaEstoque,Long> {
+
+}
